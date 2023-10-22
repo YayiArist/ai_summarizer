dc2f28790db21c9f9cc662be9303af79ca2a79bb
Yairys Aristigueta <yayi.arist.hz@gmail.com>
2023-10-22 17:27:24 -0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { logo } from "../assets";
+
+export default function Hero() {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/YayiArist")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden"/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+      Elevate your reading with Summize, a powerful article summarizer that condenses extensive articles into easily digestible summaries, streamlining your access to essential information.
+      </h2>
+    </header>
+  );
+}
