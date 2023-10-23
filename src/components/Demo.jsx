342d33b5d09aa54f222957b5ed692742f1dfd170
Yairys Aristigueta <yayi.arist.hz@gmail.com>
2023-10-23 20:04:44 -0300

    redux store config

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,12 +3,22 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 export default function Demo() {
+
+  const [article, setArticle]=useState({
+    url:'',
+    summary:'',
+  });
+
+  const handleSubmit = async (e)=>{
+e.preventDefault()
+    alert("Submitted")
+  }
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-centeritems-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -19,16 +29,25 @@ export default function Demo() {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e)=> setArticle({
+              article, url:e.target.value
+            })}
             required
             className="url_input peer"
           />
 
-          <button type="submit" className="submit_btn">↵</button>
+          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer.focus:text-gray-700">↵</button>
 
         </form>
+
+        {/* URL History */}
+
+
+
       </div>
+
+      {/* Results */}
     </section>
   );
 }
